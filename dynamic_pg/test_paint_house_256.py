5aee7a670a96569a15642e4dba941876bc145390
jakezhang <jakezhang1989@hotmail.com>
2018-11-11 22:08:17 +0800

    finished paint_house_256

--- dynamic_pg/test_paint_house_256.py
+++ dynamic_pg/test_paint_house_256.py
@@ -1,5 +1,6 @@
 import unittest
 import sys
+import copy
 
 
 class paint_house_256:
@@ -7,44 +8,49 @@ class paint_house_256:
     cd dynamic_pg
     nosetests test_paint_house_256:test_paint_house_256.test_0 -s
     https://leetcode.com/problems/paint-house/description/
+    think of steps:
+    currmins[col] = costs[row][col] + min(premins[idxl], premins[idxr]) 
+    mincost = min(currmins)
     '''
 
-    def min_cost(self, costs):
+    def min_cost_v1(self, costs):
+        '''
+        created by myself but  only beats 37%
+        reason is the copy of mins and tempmins
+        '''
         if not len(costs):
             return 0
+        mins = [costs[0][0], costs[0][1], costs[0][2]]
+        tempmins = [0]*3
+        mincost = min(mins)
+        for row in range(1, len(costs)):
+            for col in range(3):
+                print(costs[row][col])
+                idxl = (col + 1) % 3
+                idxr = (idxl+1) % 3
+                smaller = min(mins[idxl],  mins[idxr])
+                print(smaller)
+                tempmins[col] = costs[row][col] + smaller
+                print(tempmins[col])
+            mincost = min(tempmins)
+            # ucan only pdate mins after each row
+            for i in range(3):
+                mins[i] = tempmins[i]
+        return mincost
 
-        width = 3
-        mincosts = [costs[0][0], costs[0][1], costs[0][2]]
-        ret = min(mincosts)
-
-        for r, row in enumerate(costs):
-            if r == len(costs)-1:
-                break
-            temp = [sys.maxsize]*3
-            mincost = sys.maxsize
-            for c, v in enumerate(row):
-                print((r, c, v))
-                idxl = (c+1) % width
-                idxr = (idxl+1) % width
-
-                print(mincosts[c], min(costs[r+1][idxl], costs[r+1][idxr]))
-                sum1 = mincosts[c] + min(costs[r+1][idxl], costs[r+1][idxr])
-                print(sum1)
-                mincost = min(mincost, sum1)
-                print(mincost)
-
-                temp[idxl] = min(temp[idxl], v + costs[r+1][idxl])
-                temp[idxr] = min(temp[idxr], v + costs[r+1][idxr])
-
-            for i in range(width):
-                mincosts[i] = temp[i]
-            ret = mincost
-
-            for i, v in enumerate(mincosts):
-                print((i, v))
-            print('=====')
-        return ret
-
+    def min_cost_v2(self, costs):
+        '''
+        created by  valeriy2
+        https://leetcode.com/problems/paint-house/discuss/162876/Simple-Python-beats-100
+        faster because removal of copy of mins
+        '''
+        if not costs:
+            return 0;
+        for index in range(1, len(costs)):
+            costs[index][0] += min( costs[index-1][1], costs[index-1][2]) 
+            costs[index][1] += min( costs[index-1][0], costs[index-1][2]) 
+            costs[index][2] += min( costs[index-1][0], costs[index-1][1]) 
+        return min(costs[-1])
 
 class test_paint_house_256(unittest.TestCase):
     def setUp(self):
