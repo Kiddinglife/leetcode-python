05d0c2a8257a2ef24df4aa22cbf4c42e9b695272
jakezhang <jakezhang1989@hotmail.com>
2018-11-20 22:19:31 +0800

    up

---
+++ dynamic_pg/test_787_cheapest_flight.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import bisect
+import sys
+import copy
+sys.setrecursionlimit(10**5)
+
+
+'''
+https://leetcode.com/problems/cheapest-flights-within-k-stops/
+'''
+
+
+def findCheapestPrice(self, n, flights, src, dst, K):
+    """
+    :type n: int
+    :type flights: List[List[int]]
+    :type src: int
+    :type dst: int
+    :type K: int
+    :rtype: int
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
