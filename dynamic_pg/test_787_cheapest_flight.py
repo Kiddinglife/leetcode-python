5d55845dd62ea127d7ec48eed6d2f9bbc4e23059
jakezhang <jakezhang1989@hotmail.com>
2018-11-22 23:33:26 +0800

    up

--- dynamic_pg/test_787_cheapest_flight.py
+++ dynamic_pg/test_787_cheapest_flight.py
@@ -11,17 +11,34 @@ https://leetcode.com/problems/cheapest-flights-within-k-stops/
 '''
 
 
-def findCheapestPrice(self, n, flights, src, dst, K):
+def findCheapestPrice(n, flights, src, dst, K):
     """
-    :type n: int
-    :type flights: List[List[int]]
-    :type src: int
-    :type dst: int
-    :type K: int
-    :rtype: int
+    for the question that has one by one input ysyaly use interative dp
+    for the dp formular that need to compare itself usually use itreative dp
+    dp[dest][k] = 
+        min {dp[dest][k] , dp[src][k-1] + costs[src][dest]) if k > 0
+        min { costs[...src][dest] }
     """
-    pass
+    K += 1
+    dp = [[sys.maxsize ]  * n for i in range(K)]
+    for (s,d,w) in flights:
+        dp[0][d] = min(dp[0][d], w)
+    print(dp)
+    for i in range(1, K):
+        for (s,d,w) in flights:
+            v = dp[i-1][s] + w
+            dp[i][d]= min(dp[i][d], v)
+    v =   dp[K-1][dst] 
+    if sys.maxsize ==  v:
+        return -1
+    return v
 
 
 if __name__ == '__main__':
-    pass
+    n = 3
+    flights = [[0,1,100],[1,2,100],[0,2,500]]
+    src = 0
+    dst = 2
+    k = 0
+    ret = findCheapestPrice(n, flights, src, dst, k);
+    print(ret)
